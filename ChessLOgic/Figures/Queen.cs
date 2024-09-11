cc4da817f1a4fc8cd117656fa59637d51f61d2f3
mefedraw <fyodor.dubkov@gmail.com>
2024-09-12 01:14:30 +0300

    pinned piece move

--- ChessLOgic/Figures/Queen.cs
+++ ChessLOgic/Figures/Queen.cs
@@ -87,8 +87,55 @@ public override bool PossibleMove(ref IFigure[][] board, (int, int) moveStartPos
     return false; // Все другие ходы недопустимы для ферзя
 }
 
-    public Queen(char color) : base(color, FigureType.Queen)
+public override bool IsUnderAttack(IFigure[][] board, (int x, int y) position, char kingColor)
+{
+    throw new NotImplementedException();
+}
+
+public bool IsUnderAttack(IFigure[][] board, char kingColor)
+{
+    (int x, int y) kingPosition = (0, 0);
+
+    for (var column = 0; column < 8; column++) // находим союзного короля
     {
+        for (var row = 0; row < 8; row++)
+        {
+            if (board[column][row] != null)
+            {
+                if (board[column][row].Type == FigureType.King && board[column][row].Color == kingColor)
+                {
+                    kingPosition = (column, row);
+                    column = 8; 
+                    break;
+                }
+            }
+        }
     }
+
+    for (var column = 0; column < 8; column++)
+    {
+        for (var row = 0; row < 8; row++)
+        {
+            var figure = board[column][row];
+            // Если фигура противника
+            if (figure != null && figure.Color != kingColor)
+            {
+                // Проверяем, может ли фигура атаковать клетку
+                if (figure.PossibleMove(ref board, (column, row), kingPosition))
+                {
+                    figure.PossibleMove(ref board, kingPosition, (column, row));
+                    board[kingPosition.x][kingPosition.y] = new King(kingColor);
+                    return true; // Клетка под ударом
+                }
+            }
+        }
+    }
+
+    return false;
+}
+
+public Queen(char color) : base(color, FigureType.Queen)
+{
+}
     
 }
--- ChessLOgic/Figures/Rook.cs
+++ ChessLOgic/Figures/Rook.cs
@@ -6,8 +6,8 @@ public class Rook : Figure
     {
         int startX = moveStartPosition.Item1; // Горизонтальная координата (столбец)
         int startY = moveStartPosition.Item2; // Вертикальная координата (строка)
-        int endX = moveEndPosition.Item1;     // Горизонтальная координата (столбец)
-        int endY = moveEndPosition.Item2;     // Вертикальная координата (строка)
+        int endX = moveEndPosition.Item1; // Горизонтальная координата (столбец)
+        int endY = moveEndPosition.Item2; // Вертикальная координата (строка)
 
         IFigure figure = board[startX][startY];
 
@@ -51,14 +51,63 @@ public class Rook : Figure
         {
             board[startX][startY] = null;
             board[endX][endY] = figure;
+            RookDidMove = true;
             return true;
         }
 
         return false; // Если в конечной клетке фигура того же цвета, ход невозможен
     }
-    
 
-    public Rook(char color) : base(color,FigureType.Rook)
+    public override bool IsUnderAttack(IFigure[][] board, (int x, int y) position, char kingColor)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool IsUnderAttack(IFigure[][] board, char kingColor)
+    {
+        (int x, int y) kingPosition = (0, 0);
+
+        for (var column = 0; column < 8; column++) // находим союзного короля
+        {
+            for (var row = 0; row < 8; row++)
+            {
+                if (board[column][row] != null)
+                {
+                    if (board[column][row].Type == FigureType.King && board[column][row].Color == kingColor)
+                    {
+                        kingPosition = (column, row);
+                        column = 8; 
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (var column = 0; column < 8; column++)
+        {
+            for (var row = 0; row < 8; row++)
+            {
+                var figure = board[column][row];
+                // Если фигура противника
+                if (figure != null && figure.Color != kingColor)
+                {
+                    // Проверяем, может ли фигура атаковать клетку
+                    if (figure.PossibleMove(ref board, (column, row), kingPosition))
+                    {
+                        figure.PossibleMove(ref board, kingPosition, (column, row));
+                        board[kingPosition.x][kingPosition.y] = new King(kingColor);
+                        return true; // Клетка под ударом
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public bool RookDidMove { get; set; }
+
+    public Rook(char color) : base(color, FigureType.Rook)
     {
         Type = FigureType.Rook;
     }
